61bcdeb37ea82f1a7c9426fbe13884fe66ba1bcf
Kris-ArturaVhub <lckhanh2010@gmail.com>
2026-03-26 16:33:46 +0700

    Replace image source in app.py

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ def home():
     </style>
 
     <div class="container">
-         <img src="/static/Screenshot_2026-01-16-20-24-51-00_a23b203fd3aafc6dcb84e438dda678b6.jpg" alt="..." style="width: 200px; border-radius: 10px;">
+         <img src="/static/anh1.png" alt="..." style="width: 200px; border-radius: 10px;">
 
         <div class="content">
             <h1>🚀 KAvnhub_space </h1>
